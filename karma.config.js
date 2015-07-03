719fa7f5093ad40ee13ff6d2098fbbfbcced5748
Andrew Hamilton <andrew.hamilton@redspace.ca>
2015-07-03 08:30:02 -0300

    setup code coverage to use isparta so we could see coverage on untranspiled es6 code

--- karma.config.js
+++ karma.config.js
@@ -13,18 +13,14 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'jasmine-matchers'],
 
-
     // list of files / patterns to load in the browser
     files: [
         { pattern: './node_modules/phantomjs-polyfill/bind-polyfill.js', watched: false },
         { pattern: 'spec.bundle.js', watched: false }
     ],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -34,63 +30,53 @@ module.exports = function(config) {
     webpack: {
         devtool: 'inline-source-map',
         module: {
+            preLoaders: [
+                { test: /^((?!spec).)*\.js$/, exclude: [/node_modules/], loader: 'isparta-instrumenter-loader'},
+            ],
             loaders: [
                 { test: /\.js$/, exclude: [/node_modules/], loader: 'babel' },
                 { test: /\.html$/, loader: 'raw' },
                 { test: /\.scss$/, loader: 'style!css!sass' },
                 { test: /\.css$/, loader: 'style!css' }
-            ],
-            postLoaders: [ {
-                test: /^((?!spec).)*\.js$/,
-                exclude: /node_modules/,
-                loader: 'istanbul-instrumenter'
-            } ]
+            ]
         }
     },
     webpackMiddleware: {
         stats: {
           colors: true
         },
-        quiet: true
+        quiet: true //reduce spam
     },
     webpackServer: {
       noInfo: true //please don't spam the console when running in karma!
     },
 
-
     // test results reporter to use
-    // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
+
     coverageReporter: {
       reporters: [
-        { type: 'html', dir: 'dist/tests/coverage/html', subdir: 'PhantomJS' },
-        { type: 'cobertura', dir: 'dist/tests/coverage/cobertura', subdir: 'PhantomJS' }
+        { type: 'html', dir: 'dist/coverage', subdir: 'PhantomJS' },
       ]
     },
-
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     // autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
